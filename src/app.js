90b36b661ed617271ddc9bc9946f65a0c2beae7a
Aoife Woodrow <woodrow-a@email.ulster.ac.uk>
2020-10-18 15:24:51 +0100

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,21 @@
+function displayTemp(response) {
+    console.log(response.data)
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
+let city = "London";
+let apiKey = "b753aa62423f8caa495cede8803bb20c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
